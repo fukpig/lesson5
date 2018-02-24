1d4e87b0fce46ae7b06a322996ff223c2fd41949
fukpig <fukpig2@gmail.com>
2018-02-24 17:18:12 +0600

    fix

--- errors/file_not_found.rb
+++
@@ -1,7 +0,0 @@
-class FileNotFound < ArgumentError
-  attr_reader :filename
-    def initialize(filename)
-      @filename = filename
-      super("File #{filename} not found")
-  end
-end
--- errors/genre_not_found.rb
+++
@@ -1,7 +0,0 @@
-class GenreNotFound < ArgumentError
-  attr_reader :genre
-    def initialize(genre)
-      @genre = genre
-      super("Genre #{genre} not found")
-  end
-end
